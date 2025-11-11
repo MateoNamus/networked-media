f9f2311895c4680c82c8fbbba433b1c19f4357ff
MateoNamus <162263292+MateoNamus@users.noreply.github.com>
2025-11-11 12:54:43 -0500

    Add files via upload

---
+++ project4/Part1/public/main.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    refreshMessages()
+}
+
+//declaring functions inside window.onload is BAD
+async function refreshMessages() {
+    // the url I am retrieving on my server
+    const url = '/all-posts'
+    
+    //this retrieves my data from my url
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data.posts)
+}
